8fb2fc0d88adbad1cea0caba3246d731a0c3e4b6
lijanahmmed <lijanahmmed@gmail.com>
2025-04-16 21:31:09 +0600

    some correction of more advanced es6

--- forEach.js
+++ forEach.js
@@ -1,3 +1,28 @@
+const arr = [2, 3, 4, 5, 6];
+const show = a => console.log(a)
+arr.forEach(show)
+// 2
+// 3
+// 4
+// 5
+// 6
+
+arr.forEach((item, index, array) => {
+    console.log("item:", item, "index:", index, "array:", array);
+})
+// item: 2 index: 0 array: [ 2, 3, 4, 5, 6 ]
+// item: 3 index: 1 array: [ 2, 3, 4, 5, 6 ]
+// item: 4 index: 2 array: [ 2, 3, 4, 5, 6 ]
+// item: 5 index: 3 array: [ 2, 3, 4, 5, 6 ]
+// item: 6 index: 4 array: [ 2, 3, 4, 5, 6 ]
+
+let sum = 0;
+arr.forEach((item) => {
+    sum += item;
+})
+console.log(sum)
+// 20
+
 const numbers = [2, 3, 4, 5, 6];
 const double = numbers.forEach(n => n*2)
 console.log(double);
--- reduce.js
+++ reduce.js
@@ -1,4 +1,7 @@
-const numbers = [25, 36, 47, 58, 63];
-const total = numbers.reduce( (previous, current) => previous + current, 0);
-console.log(total);
-// 229
+const numbers = [2, 3, 4, 5, 6];
+const sum = numbers.reduce( (previous, current) => previous + current, 0);
+const multiply = numbers.reduce( (previous, current) => previous * current, 1);
+console.log(sum);
+// 20
+console.log(multiply);
+// 720
