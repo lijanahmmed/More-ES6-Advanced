227abc5f5d209cea0b33fec67026035fdd59ef88
Lijan Ahmmed <lijanahmmed@gmail.com>
2025-04-16 21:34:19 +0600

    Update find.js

--- find.js
+++ find.js
@@ -1,4 +1,7 @@
 const numbers = [25, 36, 47, 58, 63];
 const selected = numbers.find(n => n>40)
+const selected2 = numbers.find(n => n>40  && n%2 === 0)
 console.log(selected);
+console.log(selected2);
 // 47
+// 58
